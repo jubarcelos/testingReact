b9ed63c3db1a441edbc8d216effe9a0e13e80e5f
Julia Barcelos <julaai@gmail.com>
2021-11-30 19:32:47 -0300

    Requisito 3, com simulacao de navegacao na pagina, checa o click para checar e valida na outra page se foi checado como favorito.

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+import App from '../App';
+import userEvent from '@testing-library/user-event';
+
+describe('Test Favorite page', () => {
+  it('deve checar se aparece a mensagem quando não tem pokemon', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const favoritePokemons = screen.getByText(/No favorite pokemon found/i);
+    expect(favoritePokemons).toBeInTheDocument();
+  });
+  it('deve exibir todos os cards de pokemons', () => {
+    renderWithRouter(<App />);
+    const moreDetails = screen.getByRole('link', { name: /More details/i });
+    expect(moreDetails).toBeInTheDocument();
+    userEvent.click(moreDetails);
+    const favoritado = screen.getByRole('checkbox', { name: /pokémon favoritado/i });
+    userEvent.click(favoritado);
+    const favoritedPokemons = screen.getByRole('link', { name: /Favorite/i });
+    userEvent.click(favoritedPokemons);
+    const pokemon = screen.getByText(/average/i);
+    expect(pokemon).toBeDefined();
+  });
+});
