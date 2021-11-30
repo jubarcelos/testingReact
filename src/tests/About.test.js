e930489962702dcf7dc409d51a5cd2991c93adba
Julia Barcelos <julaai@gmail.com>
2021-11-30 18:36:10 -0300

    change local of renderWithRouter

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,25 +1,27 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
-import renderWithRouter from '../components/renderWithRouter';
+import renderWithRouter from './renderWithRouter';
 import About from '../components/About';
 
-test('renderiza a page about e mostra o título', () => {
-  renderWithRouter(<About />);
-  const title = screen.getByRole('heading',
-    { level: 2, name: 'About Pokédex' });
-  expect(title).toBeInTheDocument();
-});
+describe('Test about page', () => {
+  it('renderiza a page about e mostra o título', () => {
+    renderWithRouter(<About />);
+    const title = screen.getByRole('heading',
+      { level: 2, name: 'About Pokédex' });
+    expect(title).toBeInTheDocument();
+  });
 
-test('mostra o primeiro paragrafo', () => {
-  renderWithRouter(<About />);
-  const paragrafo = screen.getAllByText(/Pokédex/i);
-  expect(paragrafo.length).toBe(2);
-});
+  it('mostra o primeiro paragrafo', () => {
+    renderWithRouter(<About />);
+    const paragrafo = screen.getAllByText(/Pokédex/i);
+    expect(paragrafo.length).toBe(2);
+  });
 
-test('mostra a imagem https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png', () => {
-  renderWithRouter(<About />);
-  const img = screen.getByRole('img');
-  expect(img).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
-});
+  it('mostra a imagem https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png', () => {
+    renderWithRouter(<About />);
+    const img = screen.getByRole('img');
+    expect(img).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
 
-// referencia: https://dev.to/raphaelchaula/a-simple-image-test-in-react-3p6f
+  // referencia: https://dev.to/raphaelchaula/a-simple-image-test-in-react-3p6f
+});
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,50 +1,52 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import renderWithRouter from '../components/renderWithRouter';
+import renderWithRouter from './renderWithRouter';
 import App from '../App';
 
-it('deve renderizar o componente App e checar a existencia dos links', () => {
-  renderWithRouter(<App />);
-  const homeLink = screen.getByRole('link', { name: /Home/i });
-  expect(homeLink).toBeInTheDocument();
+describe('Test app page', () => {
+  it('deve renderizar o componente App e checar a existencia dos links', () => {
+    renderWithRouter(<App />);
+    const homeLink = screen.getByRole('link', { name: /Home/i });
+    expect(homeLink).toBeInTheDocument();
 
-  const aboutLink = screen.getByRole('link', { name: /about/i });
-  expect(aboutLink).toBeInTheDocument();
+    const aboutLink = screen.getByRole('link', { name: /about/i });
+    expect(aboutLink).toBeInTheDocument();
 
-  const favoritesLink = screen.getByRole('link', { name: /Favorite Pokémons/i });
-  expect(favoritesLink).toBeInTheDocument();
-});
+    const favoritesLink = screen.getByRole('link', { name: /Favorite Pokémons/i });
+    expect(favoritesLink).toBeInTheDocument();
+  });
 
-it('deve verificar a rota do link home', () => {
-  const { history } = renderWithRouter(<App />);
-  const homeLink = screen.getByRole('link', { name: /home/i });
-  const { pathname } = history.location;
-  userEvent.click(homeLink);
-  expect(pathname).toBe('/');
-});
-it('deve verificar a rota do link about', () => {
-  const { history } = renderWithRouter(<App />);
-  const aboutLink = screen.getByRole('link', { name: /about/i });
-  userEvent.click(aboutLink);
-  const { pathname } = history.location;
-  expect(pathname).toBe('/about');
-});
+  it('deve verificar a rota do link home', () => {
+    const { history } = renderWithRouter(<App />);
+    const homeLink = screen.getByRole('link', { name: /home/i });
+    const { pathname } = history.location;
+    userEvent.click(homeLink);
+    expect(pathname).toBe('/');
+  });
+  it('deve verificar a rota do link about', () => {
+    const { history } = renderWithRouter(<App />);
+    const aboutLink = screen.getByRole('link', { name: /about/i });
+    userEvent.click(aboutLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
 
-it('deve verificar a rota do link favorites', () => {
-  const { history } = renderWithRouter(<App />);
-  const favoritesLink = screen.getByRole('link', { name: /Favorite/i });
-  expect(favoritesLink).toBeInTheDocument();
+  it('deve verificar a rota do link favorites', () => {
+    const { history } = renderWithRouter(<App />);
+    const favoritesLink = screen.getByRole('link', { name: /Favorite/i });
+    expect(favoritesLink).toBeInTheDocument();
 
-  userEvent.click(favoritesLink);
-  const { pathname } = history.location;
-  expect(pathname).toBe('/favorites');
-});
+    userEvent.click(favoritesLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
 
-it('deve verificar a rota do link NotFound', () => {
-  const { history } = renderWithRouter(<App />);
-  history.push('/julia');
-  const title = screen.getByRole('heading',
-    { level: 2, name: 'Page requested not found Crying emoji' });
-  expect(title).toBeInTheDocument();
+  it('deve verificar a rota do link NotFound', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/julia');
+    const title = screen.getByRole('heading',
+      { level: 2, name: 'Page requested not found Crying emoji' });
+    expect(title).toBeInTheDocument();
+  });
 });
