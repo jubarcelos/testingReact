022624ee670df91ea4c0c21073c843e0964de2c4
Julia Barcelos <julaai@gmail.com>
2021-12-01 20:24:07 -0300

    Requisito 7 - Complet checkes, but need some changes to stay better.

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -5,29 +5,6 @@ import renderWithRouter from './renderWithRouter';
 import Pokemon from '../components/Pokemon';
 import pokemons from '../data';
 
-// const pokemom = {
-//   id: 25,
-//   name: 'Pikachu',
-//   type: 'Electric',
-//   averageWeight: {
-//     value: '6.0',
-//     measurementUnit: 'kg',
-//   },
-//   image: 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
-//   moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)',
-//   foundAt: [
-//     {
-//       location: 'Kanto Viridian Forest',
-//       map: 'https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png',
-//     },
-//     {
-//       location: 'Kanto Power Plant',
-//       map: 'https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png',
-//     },
-//   ],
-//   summary: 'This intelligent Pokémon roasts hard berries...',
-// };
-
 describe('Test Pokemon page', () => {
   it('dados renderizados do pokémom', () => {
     renderWithRouter(<Pokemon isFavorite pokemon={ pokemons[0] } />);
--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,44 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+import pokemons from '../data';
+
+describe('Test PokemonDetails from App', () => {
+  it('deve mostrar o nome e detalhes do pokemom e não ter o button details', () => {
+    renderWithRouter(<App />);
+    const moreDetails = screen.getByRole('link', { name: /More details/i });
+    userEvent.click(moreDetails);
+
+    const pikachuName = screen.getByRole('heading',
+      { level: 2, name: 'Pikachu Details' });
+    expect(pikachuName).toBeInTheDocument();
+    const subTitle = screen.getByRole('heading', { level: 2, name: 'Summary' });
+    expect(moreDetails).not.toBeInTheDocument();
+    expect(subTitle).toBeInTheDocument();
+    const paragraph = screen.getByText(/This intelligent Pokémon roasts hard berries/i);
+    expect(paragraph).toBeInTheDocument();
+
+    const mapTitle = screen.getByRole('heading',
+      { level: 2, name: 'Game Locations of Pikachu' });
+    expect(mapTitle).toBeInTheDocument();
+    const local1 = screen.getByText(/Kanto Viridian Forest/i);
+    const local2 = screen.getByText(/Kanto Power Plant/i);
+    expect(local2).toBeInTheDocument();
+    expect(local1).toBeInTheDocument();
+    const localImg = screen.getAllByRole('img', { name: /Pikachu location/i });
+    expect(localImg[0]).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png');
+    expect(localImg[0]).toHaveAttribute('alt', 'Pikachu location');
+    expect(localImg[1]).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png');
+    expect(localImg[1]).toHaveAttribute('alt', 'Pikachu location');
+
+    const favoritado = screen.getByRole('checkbox', { name: /pokémon favoritado?/i });
+    userEvent.click(favoritado);
+    const Img = screen.getByRole('img', { name: /Pikachu is marked as favorit/i });
+    expect(Img).toHaveAttribute('src', '/star-icon.svg');
+    expect(Img).toBeInTheDocument();
+    userEvent.click(favoritado);
+    expect(Img).not.toBeInTheDocument();
+  });
+});
